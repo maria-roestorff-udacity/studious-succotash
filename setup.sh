53b2b146df9c904336f951a109d830a5ce7e1964
Maria Roestorff <maria.roestorff@liberty-it.co.uk>
2023-12-25 10:35:19 +0000

    initial commit

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+export DATABASE_URL="postgresql://postgres:tydelikeUdacity@localhost:5432/hsdb"
+export EXCITED="true"
+echo "setup.sh script executed successfully!"
